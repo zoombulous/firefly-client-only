637d0b43d694c4c1160112774aa8b17e0ff2cbf2
zoombulous <44047136+zoombulous@users.noreply.github.com>
2021-01-05 14:40:59 -0800

    test

--- src/components/FireflyGame.vue
+++ src/components/FireflyGame.vue
@@ -13524,7 +13524,7 @@ export default {
         }
     },
       created() {
-        this.socket = io('https://mighty-plateau-03052.herokuapp.com/' , { transport : ['websocket'] });
+        this.socket = io('testtesttest' , { transport : ['websocket'] });
 this.socket.on('chat-message', (data) => {
             this.messages.push({ message: data.message, type: 1, by: data.user })
             this.typing = false
